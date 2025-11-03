7d0bec198fbbff7ef9507af5b25fcb2262c77ce8
ShadowPC <ShadowPC@SHADOWBOOK>
2025-11-03 00:00:03 -0600

    Minor change

--- win32extended.h
+++ win32extended.h
@@ -133,7 +133,7 @@ namespace stdx {
         ShowWindow(hwnd, SW_SHOW);
     }
 
-#undef MessageBox
+    #undef MessageBox
     namespace MessageBox
     {
         enum class Type {
@@ -316,4 +316,169 @@ namespace stdx {
             return found;
         }
     }
+
+    template<typename K, typename V>
+    class ordered_map {
+    public:
+        using key_type = K;
+        using mapped_type = V;
+        using value_type = std::pair<const K, V>;
+        using size_type = size_t;
+
+        using container_type = std::vector<std::pair<K, V>>;
+        using iterator = typename container_type::iterator;
+        using const_iterator = typename container_type::const_iterator;
+
+        ordered_map() = default;
+        ordered_map(const ordered_map&) = default;
+        ordered_map& operator=(const ordered_map&) = default;
+        ordered_map(ordered_map&&) noexcept = default;
+        ordered_map& operator=(ordered_map&&) noexcept = default;
+
+        // element access
+        V& operator[](const K& key) {
+            auto it = m_index.find(key);
+            if (it == m_index.end()) {
+                return push_back(key, V{})->second;
+            }
+            return m_data[it->second].second;
+        }
+
+        V& operator[](K&& key) {
+            auto it = m_index.find(key);
+            if (it == m_index.end()) {
+                return push_back(std::move(key), V{})->second;
+            }
+            return m_data[it->second].second;
+        }
+
+        V& at(const K& key) { return m_data.at(m_index.at(key)).second; }
+        const V& at(const K& key) const { return m_data.at(m_index.at(key)).second; }
+
+        // modifiers
+        std::pair<iterator, bool> insert(iterator pos, const value_type& value) {
+            auto it = m_index.find(value.first);
+            if (it != m_index.end())
+                return { m_data.begin() + it->second, false };
+
+            auto insertPos = m_data.insert(pos, { value.first, value.second });
+            rebuild_index();
+            return { insertPos, true };
+        }
+
+        std::pair<iterator, bool> insert(iterator pos, value_type&& value) {
+            auto it = m_index.find(value.first);
+            if (it != m_index.end())
+                return { m_data.begin() + it->second, false };
+
+            auto insertPos = m_data.insert(pos, std::move(value));
+            rebuild_index();
+            return { insertPos, true };
+        }
+
+        iterator push_back(const K& key, V&& value) {
+            auto it = m_index.find(key);
+            if (it != m_index.end())
+                return m_data.begin() + it->second;
+
+            m_data.emplace_back(key, std::move(value));
+            m_index[key] = m_data.size() - 1;
+            return std::prev(m_data.end());
+        }
+
+        iterator push_back(K&& key, V&& value) {
+            auto it = m_index.find(key);
+            if (it != m_index.end())
+                return m_data.begin() + it->second;
+
+            m_data.emplace_back(std::move(key), std::move(value));
+            m_index[m_data.back().first] = m_data.size() - 1;
+            return std::prev(m_data.end());
+        }
+
+        template<typename... Args>
+        iterator emplace_back(const K& key, Args&&... args) {
+            auto it = m_index.find(key);
+            if (it != m_index.end())
+                return m_data.begin() + it->second;
+
+            m_data.emplace_back(key, V(std::forward<Args>(args)...));
+            m_index[key] = m_data.size() - 1;
+            return std::prev(m_data.end());
+        }
+
+        template<typename... Args>
+        std::pair<iterator, bool> emplace(const K& key, Args&&... args) {
+            auto it = m_index.find(key);
+            if (it != m_index.end())
+                return { m_data.begin() + it->second, false };
+
+            m_data.emplace_back(key, V(std::forward<Args>(args)...));
+            m_index[key] = m_data.size() - 1;
+            return { std::prev(m_data.end()), true };
+        }
+
+        void erase(const K& key) {
+            auto it = m_index.find(key);
+            if (it == m_index.end()) return;
+
+            size_t idx = it->second;
+            m_data.erase(m_data.begin() + idx);
+            m_index.erase(it);
+            rebuild_index();
+        }
+
+        iterator erase(iterator pos) {
+            if (pos == m_data.end()) return m_data.end();
+
+            const K& key = pos->first;
+            m_index.erase(key);
+            auto next = m_data.erase(pos);
+            rebuild_index();
+            return next;
+        }
+
+        void clear() noexcept {
+            m_data.clear();
+            m_index.clear();
+        }
+
+        // lookup
+        iterator find(const K& key) {
+            auto it = m_index.find(key);
+            if (it == m_index.end()) return m_data.end();
+            return m_data.begin() + it->second;
+        }
+
+        const_iterator find(const K& key) const {
+            auto it = m_index.find(key);
+            if (it == m_index.end()) return m_data.end();
+            return m_data.begin() + it->second;
+        }
+
+        bool contains(const K& key) const noexcept { return m_index.find(key) != m_index.end(); }
+        size_type count(const K& key) const noexcept { return m_index.count(key); }
+
+        // iteration
+        iterator begin() noexcept { return m_data.begin(); }
+        iterator end() noexcept { return m_data.end(); }
+        const_iterator begin() const noexcept { return m_data.begin(); }
+        const_iterator end() const noexcept { return m_data.end(); }
+        const_iterator cbegin() const noexcept { return m_data.cbegin(); }
+        const_iterator cend() const noexcept { return m_data.cend(); }
+
+        // capacity
+        bool empty() const noexcept { return m_data.empty(); }
+        size_type size() const noexcept { return m_data.size(); }
+
+    private:
+        void rebuild_index() {
+            m_index.clear();
+            for (size_t i = 0; i < m_data.size(); ++i)
+                m_index[m_data[i].first] = i;
+        }
+
+        container_type m_data;
+        std::unordered_map<K, size_t> m_index;
+    };
 }
